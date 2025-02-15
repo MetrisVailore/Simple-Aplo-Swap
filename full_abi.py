64bac7e045df011a113235e3198a742bf1ad2a9d
MetrisVailore <122095030+MetrisVailore@users.noreply.github.com>
2025-02-15 08:57:26 +0100

    Update full_abi.py

--- full_abi.py
+++ full_abi.py
@@ -1,16 +1,79 @@
 abi = [
-  {'inputs': [
-    {'internalType': 'address', 'name': 'wethAddress', 'type': 'address'}, 
-    {'internalType': 'uint256', 'name': 'initialMinimumLiquidity', 'type': 'uint256'}, 
-    {'internalType': 'uint256', 'name': 'initialMaxSwapFee', 'type': 'uint256'}
-  ], 
-   'stateMutability': 'nonpayable', 'type': 'constructor'}, 
-  {'inputs': [
-    {'internalType': 'uint256', 'name': 'newMinimumLiquidity', 'type': 'uint256'}
-  ], 
-   'name': 'setMinimumLiquidity', 'outputs': [], 'stateMutability': 'nonpayable', 'type': 'function'}, 
-  {'anonymous': False, 'inputs': 
-   [{'indexed': True, 'internalType': 'address', 'name': 'sender', 'type': 'address'}, 
-    {'indexed': True, 'internalType': 'bytes32', 'name': 'poolId', 'type': 'bytes32'}, 
-    {'indexed': False, 'internalType': 'address', 'name': 'tokenIn', 'type': 'address'}, 
-    {'indexed': False, 'internalType': 'uint256', 'name': 'amountIn', 'type': 'uint256'}, {'indexed': False, 'internalType': 'address', 'name': 'tokenOut', 'type': 'address'}, {'indexed': False, 'internalType': 'uint256', 'name': 'amountOut', 'type': 'uint256'}], 'name': 'Swap', 'type': 'event'}]
+  {
+    "type": "constructor",
+    "stateMutability": "nonpayable",
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "wethAddress",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "initialMinimumLiquidity",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "initialMaxSwapFee",
+        "type": "uint256"
+      }
+    ]
+  },
+  {
+    "type": "function",
+    "name": "setMinimumLiquidity",
+    "stateMutability": "nonpayable",
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "newMinimumLiquidity",
+        "type": "uint256"
+      }
+    ],
+    "outputs": []
+  },
+  {
+    "type": "event",
+    "name": "Swap",
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "sender",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "bytes32",
+        "name": "poolId",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "tokenIn",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amountIn",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "tokenOut",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amountOut",
+        "type": "uint256"
+      }
+    ]
+  }
+]
